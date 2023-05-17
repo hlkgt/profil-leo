703696f628cedb250efe192be6a5709a6225c619
hlkgt <haihaioell@gmail.com>
2023-05-17 13:48:27 +0700

    create about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path={"/"} element={<Home />} />
+        <Route path={"/about"} element={<About />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/About.jsx
@@ -0,0 +1,38 @@
+import { Fragment } from "react";
+import Navbar from "../components/Navbar";
+
+const About = () => {
+  return (
+    <Fragment>
+      <Navbar />
+      <div className="w-full bg-slate-900">
+        <div className="min-h-screen container mx-auto px-8 flex justify-center items-center">
+          <div className="grid grid-cols-1 lg:grid-cols-2">
+            <div className="col-span-1">
+              <img src="" alt={"foto-saya"} title={"foto-saya"} />
+            </div>
+            <div className="col-span-1">
+              <h1 className={"text-4xl mb-2 font-bold text-slate-200"}>
+                Tentang Saya
+              </h1>
+              <p className={"text-slate-400 font-medium text-justify"}>
+                Saya adalah alumni mahasiswa SMKN 1 Dlanggu yang lulus pada
+                tahun ini, pada tanggal 5 Mei 2023, pada tahun 2023 ini berumur
+                18 tahun, saya adalah seorang Fullstack developer, pernah magang
+                di Lab Akselerasi didaerah Kabupaten Gondang di bidang coding /
+                programming, disana saya mempelajari banyak hal seperti
+                mempelajari dasar dasar tentang pemrograman, saya juga menjadi
+                Fullstack Developer karena menjadi murid magang disana selama 6
+                bulan dari Bulan Juni 2022 - Desember 2022, dan sampai sekarang
+                masih saya lanjutkan untuk mempelajari hal baru, dan pernah juga
+                diajarkan untuk mendesain dari aplikasi Figma
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default About;
